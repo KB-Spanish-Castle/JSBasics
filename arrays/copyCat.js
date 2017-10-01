62d2c8e5b0e4f421eb6df6ddc8332fefe04f30ea
KB-Spanish-Castle <kylegrantb@gmail.com>
2017-10-01 10:07:42 -0600

    Now looking at copyCat.test.js as an example

--- arrays/copyCat.js
+++ arrays/copyCat.js
@@ -2,6 +2,23 @@
 // returns the second and third index values.
 
 
+
+//I had done slice that was incorrect!
+
+var copyCat;
+
+//var arr2 = [12, 42, 36, 51, 96];
+
+function copyCat(arr)
+{
+
+
+  copycatArray = catArray.push(arr[1], arr[2]);
+  
+  return copycatArray;
+}
+
+/*
 var array2 =[];
 array2[0] = arr[3];
 array2[1] = arr[4];
@@ -9,7 +26,7 @@ array2[1] = arr[4];
 var copyCat= array2.map(function(x) {
   return x;
 });
-
+*/
 
 // DO NOT DELETE
 module.exports = copyCat;
