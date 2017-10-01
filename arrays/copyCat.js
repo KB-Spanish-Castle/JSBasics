08f30007bc1bf183bcf2d77a425364b4334f4f56
KB-Spanish-Castle <kylegrantb@gmail.com>
2017-09-30 18:36:22 -0600

    Getting closer for problem 7

--- arrays/copyCat.js
+++ arrays/copyCat.js
@@ -1,15 +1,29 @@
 // Create a function called copyCat that takes in an array, copies, and
 // returns the second and third index values.
 
-var copyCat;
 
 
-function copyCat(arr, copyCat)
-{
-  copyCat = arr[2];
-  copyCat = arr[3];
-  return copyCat;
-}
+/*
+
+Your copyCat is not doing what was asked for.
+They did not ask you to add elements to the array
+It said to make a new array with two elements.
+
+
+copyCat is asking you to construct a simple array with only two elements.
+Please study how to make a simple array.
+It is the most basic of javascript array syntax.
+*/
+
+
+var array2 =[];
+array2[0] = arr[3];
+array2[1] = arr[4];
+
+var copyCat= array2.map(function(x) {
+  return x;
+});
+
 
 //I had done slice that was incorrect!
 // DO NOT DELETE
