100861baa0e95bda52ce3795d4ebad00bcdce769
KB-Spanish-Castle <kylegrantb@gmail.com>
2017-10-01 18:25:52 -0600

    Finished reverseString.js

--- arrays/copyCat.js
+++ arrays/copyCat.js
@@ -1,7 +1,17 @@
 // Create a function called copyCat that takes in an array, copies, and
 // returns the second and third index values.
 
+/*
 
+Your copyCat is not doing what was asked for.
+They did not ask you to add elements to the array
+It said to make a new array with two elements.
+
+
+copyCat is asking you to construct a simple array with only two elements.
+Please study how to make a simple array.
+It is the most basic of javascript array syntax.
+*/
 
 //I had done slice that was incorrect!
 
@@ -9,13 +19,15 @@ var copyCat;
 
 //var arr2 = [12, 42, 36, 51, 96];
 
-function copyCat(arr)
+function copyCat(arr, upFrontArray, copycatArray)
 {
-
-
-  copycatArray = catArray.push(arr[1], arr[2]);
+  upFrontArray = arr;
+  //copycatArray[0] = arr[2];
+  //copycatArray[1] = arr[3];
+  //upFrontArray.push(arr);
+  //copycatArray.push(arr[2], arr[3]);
   
-  return copycatArray;
+  return copyCat;
 }
 
 /*
--- arrays/reverseString.js
+++ arrays/reverseString.js
@@ -2,5 +2,15 @@
 
 var reverseString;
 
+function reverseString(val)
+{
+  var splitString = val.split("");
+  var reverseArray = splitString.reverse();
+  var joinArray = reverseArray.join("");
+  return joinArray;
+}
+
+
 // DO NOT DELETE
 module.exports = reverseString;
+//https://medium.freecodecamp.org/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb
