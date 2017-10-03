d9f6a7c1c1f2d9c07843bb6c4cea2eeb14803a92
KB-Spanish-Castle <kylegrantb@gmail.com>
2017-10-03 16:24:18 -0600

    I just simlified copyCat.js

--- arrays/copyCat.js
+++ arrays/copyCat.js
@@ -1,23 +1,17 @@
 // Create a function called copyCat that takes in an array, copies, and
 // returns the second and third index values.
 
-
-
 var copyCat;
-
-var arr2 = [1, 2, 3, 4, 5];
 
 function copyCat(arr)
 {
-  upFrontArray = arr;
   var arr3 = [];
+  
   arr3[0] = arr[2];
   arr3[1] = arr[3];
   
   return arr3;
 }
-
-
 
 // DO NOT DELETE
 module.exports = copyCat;
