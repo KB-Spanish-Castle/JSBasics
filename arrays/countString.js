8fe65c29070c876710a01376e614fca75350b958
KB-Spanish-Castle <kylegrantb@gmail.com>
2017-10-06 14:19:17 -0600

    Finished countString.js

--- arrays/countString.js
+++ arrays/countString.js
@@ -4,40 +4,19 @@
 
 var searchValue = "";
 var importWord = "";
-//var toLowerCaseWord = importWord.toLowerCase();
-function countString(importWord, searchValue)
-{
+
+function countString(importWord, searchValue) {
   var countValue = 0;
 
-  for(var wom = 0; wom < importWord.length; wom++)
-  {   
-    if(importWord[wom].toLowerCase() == searchValue.toLowerCase())
-    {
-      countValue++;      
+  for (var wom = 0; wom < importWord.length; wom++) {
+    if (importWord[wom].toLowerCase() == searchValue.toLowerCase()) {
+      countValue++;
     }
 
   }
   console.log("OUR toLowerCaseWord IS : " + importWord);
   return countValue;
 }
-
-/*
-function countString(valString)
-{
-  
-  var counter = 0;
-  
-  for (var i = 0; i < valString.length; i++) {
-    var index_of_sub = valString.indexOf(input_letter, i);
-  
-    if (index_of_sub > -1) {
-      counter++;
-      i = index_of_sub;
-    }
-  }
-  return 3;
-}
-*/
 
 // DO NOT DELETE
 module.exports = countString;
