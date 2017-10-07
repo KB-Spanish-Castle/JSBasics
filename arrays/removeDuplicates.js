2b3ea82d38b9ddc96d7ef6ed8306a842e885ae82
KB-Spanish-Castle <kylegrantb@gmail.com>
2017-10-07 14:46:07 -0600

    All are now passing

--- arrays/removeDuplicates.js
+++ arrays/removeDuplicates.js
@@ -1,6 +1,19 @@
-// Write a JavaScript function removeDuplicates that removes duplicate values fom an array.
+// Write a JavaScript function removeDuplicates that 
+//removes duplicate values fom an array.
 
 var removeDuplicates;
+var noRepeats = [];
+//var result = [1, 2, 3];
+function removeDuplicates(arr) {
+  arr.forEach(function (elementNumber) {
+    console.log("ARR is : " + elementNumber);
+    if (noRepeats.indexOf(elementNumber) < 0) {
+      noRepeats.push(elementNumber);
+    }
+
+  });
+  return noRepeats;
+}
 
 // DO NOT DELETE
 module.exports = removeDuplicates;
