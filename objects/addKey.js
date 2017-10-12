a51bcf2161d9cee7308358ec2cb4300e0454af5a
KB-Spanish-Castle <kylegrantb@gmail.com>
2017-10-12 14:13:33 -0600

    After break

--- objects/addKey.js
+++ objects/addKey.js
@@ -1,20 +1,16 @@
 // Create a function called addKey that takes in an object and adds a new key
-// name weight and assigns it a value. Return the object.
+// named weight and assigns it a value. Return the object.
 
-var animal;
 var addKey;
-var myAnimals = {animalType: "Tiger"};
 
 
 function addKey(myAnimals) {
-  myAnimals = {
-    animalType: "Lizzard",
-    animalName: "Aligator",
-    animalAge: 5
 
-  }
+  myAnimals.weight = 250;
+  return {
+    myAnimals
+  };
 
-  return addKey;
 }
 
 // DO NOT DELETE
--- objects/changeName.js
+++ objects/changeName.js
@@ -1,8 +1,14 @@
 // Create a function called changeName that takes in an object and a name
 // value and replaces the object name with the given name value. Return the new object.
 
-var animal;
+
 var changeName;
+var animalName;
+
+function changeName(animal, lion) {
+  console.log(animal);
+  return animal;
+}
 
 // DO NOT DELETE
 module.exports = changeName;
