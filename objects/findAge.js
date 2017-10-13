df7d8ddbe152f40471ad6b7a23d26236af5f62c6
KB-Spanish-Castle <kylegrantb@gmail.com>
2017-10-13 16:11:52 -0600

    Friday

--- objects/findAge.js
+++ objects/findAge.js
@@ -1,8 +1,12 @@
 // Create a function called findAge that takes in the
 // animal object and returns the age.
 
-var animal;
+var animal = {};
 var findAge;
 
+function findAge() {
+  animal = { age: 10 }
+  return findAge.age;
+}
 // DO NOT DELETE
 module.exports = findAge;
--- objects/findSpecies.js
+++ objects/findSpecies.js
@@ -2,6 +2,18 @@
 // the array and returns an array of all of the species values for each object.
 
 var findSpecies;
+var newArray = [];
+//var speciesList=[];
+
+function findSpecies(arr) {
+
+  for (iy = 0; iy < arr.length; iy++) {
+    newArray.push(arr[iy].species);
+  }
+  console.log(newArray + " ");
+  
+  return newArray;
+}
 
 // DO NOT DELETE
 module.exports = findSpecies;
